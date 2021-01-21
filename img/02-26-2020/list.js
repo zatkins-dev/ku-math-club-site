6818d7ff262ae5bd240aed73f6f3a51971294d76
Zachary Atkins <atkins12@llnl.gov>
2021-01-21 13:03:21 -0600

    current website

---
+++ img/02-26-2020/list.js
@@ -0,0 +1,14 @@
+files = [
+    "Photo Feb 26, 5 27 24 PM.jpg",
+    "Photo Feb 26, 5 27 37 PM.jpg",
+    "Photo Feb 26, 5 27 45 PM.jpg",
+    "Photo Feb 26, 5 27 54 PM.jpg",
+    "Photo Feb 26, 5 28 23 PM.jpg",
+    "Photo Feb 26, 5 29 08 PM.jpg",
+    "Photo Feb 26, 5 29 25 PM.jpg",
+    "Photo Feb 26, 5 30 46 PM.jpg",
+    "Photo Feb 26, 5 30 48 PM.jpg",
+    "Photo Feb 26, 5 31 36 PM.jpg",
+    "Photo Feb 26, 5 31 38 PM.jpg",
+    "Photo Feb 26, 5 54 55 PM.jpg"
+]
